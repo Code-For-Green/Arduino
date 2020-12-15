e315e4f9e0af751a197a7d7676c380549e5efcfe
Crawcik <crawcik@gmail.com>
2020-12-15 15:54:02 +0100

    Better minute handler

--- licznik/licznik.ino
+++ licznik/licznik.ino
@@ -3,13 +3,15 @@
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Ustawienie adresu ukladu na 0x27
 int period = 1000;
-int period2 = 60000;
 unsigned long time_now = 0;
-unsigned long time_now2 = 0;
 int previousOutput;
 int flow;
-int flow_m;
-int flowMinutes;
+
+int minute_data[60] = { };
+int avalible_array = 0;
+
+int time_delay;
+
 //Narazie takie parametry bo nwm jakie na pompie będą dane
 
 void setup() {
@@ -18,6 +20,7 @@ void setup() {
   lcd.setCursor(0,0); // Ustawienie kursora w pozycji 0,0 (pierwszy wiersz, pierwsza kolumna)
   previousOutput = 0;
   flow = 0;
+  time_delay = 50;
   Serial.begin(9600);
   pinMode(2, INPUT);
   for(int i = 100; i < 0; i--)
@@ -30,30 +33,38 @@ void setup() {
 }
 
 void loop() {
-  int del = analogRead(A0);
-
-  //Co minutę
-  if(millis() >= time_now2 + period2){
-    time_now2 += period2;
-    flowMinutes = flow_m;
-    flow_m = 0;
-  }
-
+  //Ustawianie delayu  
+   while(Serial.available()) {
+      time_delay = Serial.read();
+   }
   //Co sekundę
-  if(millis() >= time_now + period){
-        time_now += period;
-        Serial.println(flow);
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("Flow: ");
-        lcd.print(flow);
-        lcd.print("/s ");
-        lcd.print(flowMinutes);
-        lcd.print("/m");
-        lcd.setCursor(0,1);
-        lcd.print("Delay: ");
-        lcd.print(del);
-        flow = 0;
+  if(millis() >= time_now + period)
+  {
+      int sum = 0;
+      for(int &data : minute_data)
+      {
+        sum += data;
+      }
+      if(avalible_array == 60)
+        avalible_array = 0;
+      minute_data[avalible_array] = flow;
+      avalible_array++;
+      time_now += period;
+      
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Flow: ");
+      lcd.print(flow);
+      lcd.print("/s ");
+      lcd.print(sum);
+      lcd.print("/m");
+      lcd.setCursor(0,1);
+      lcd.print("Delay: ");
+      lcd.print(time_delay);
+      lcd.setCursor(0,2);
+      lcd.print("Moisture: ");
+      lcd.print(analogRead(A1));
+      flow = 0;
   }
 
   //Obliczanie ticków
@@ -62,7 +73,6 @@ void loop() {
   {
     previousOutput = output;
     flow++;
-    flow_m++;
   }
-  delay(del);
+  delay(time_delay);
 }
